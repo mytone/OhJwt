f21dbf75a8ff96830f27dc39d699c68efe1418fe
zou_chao824 <zou_chao824@163.com>
2023-09-01 15:02:12 +0800

    添加项目文件。

---
+++ Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace OhJwt.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+        [HttpGet("Login")]
+        public ActionResult Login(string user, string password)
+        {
+            if (user != "mytone" || password != "mytone")
+            {
+                return Ok("Error!");
+            }
+            string userid = "55";
+            string token=GenerateToke(userid,user,password);
+            return Ok(token);
+        }
+
+        /// <summary>
+        /// function deal token and return token
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        private string GenerateToke(string userid,string user, string password)
+        {
+            string secret = "1235234242384721424214328789"; //token , key must >16 chars
+            string issuer = "mytone";
+            string audience = "mytoneto";
+            var securityKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var sigingCredentials=new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new Claim[]
+            {
+                new Claim("userid", userid),
+                new Claim("user",user)
+            };
+            SecurityToken securityToken = new JwtSecurityToken(
+                issuer:issuer,
+                audience:audience,
+                claims:claims,
+                signingCredentials:sigingCredentials,
+                expires:DateTime.Now.AddMinutes(30)
+            );
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+        }
+
+
+        [HttpGet("GetUserInfo")]
+        [Authorize]
+        public ActionResult GetUserInfo() 
+        {
+            return Ok("Token access!");
+        }
+
+
+        [HttpGet("TestNoAuth")]
+        public ActionResult TestNoAuth()
+        {
+            return Content("No Token but Access!");
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
+
+namespace OhJwt;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+    public IConfiguration Configuration { get; }
+
+
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //添加jwt验证：
+        services.AddControllers();
+
+        #region
+        string secret = "1235234242384721424214328789";   //token , key must >16 chars
+        string issuer = "mytone";
+        string audience = "mytoneto";
+        services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+                        ValidateIssuer = true,  //是否验证颁发者
+                        ValidIssuer = issuer,
+                        ValidateAudience = true,
+                        ValidAudience = audience,
+                        RequireExpirationTime = true,
+                        ValidateLifetime =true,
+                    };
+                });
+        #endregion
+
+        services.AddCors(options => {
+            options.AddPolicy("any", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+        });
+
+
+
+        // 按照文档，这两个是3.x版的breaking change，要加上
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+       // services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+        services.AddControllersWithViews();
+        services.AddMvc();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+    }
+
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+
+        app.UseStaticFiles();
+
+
+
+        app.UseRouting();
+
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+
+
+
+    }
+
+
+
+}
